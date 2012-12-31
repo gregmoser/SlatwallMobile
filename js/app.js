03cb1bc9a842781410671cae54c56e347440ddf8
Greg Moser <greg@gregmoser.com>
2012-12-31 13:13:12 -0800

    Lots of updates

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/dashboard', {templateUrl: 'views/dashboard.html', controller: Orders});
     $routeProvider.when('/products', {templateUrl: 'views/products.html', controller: Products});
     $routeProvider.when('/orders', {templateUrl: 'views/orders.html', controller: Orders});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/dashboard'});
   }]);
