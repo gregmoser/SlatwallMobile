afbf11c1afc5051658312d15471b86a57e59bcdd
Greg Moser <greg@gregmoser.com>
2012-12-31 18:41:01 -0800

    Updated some layout stuff.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,11 @@
 angular.module('slatwallApp', []).
   config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/dashboard', {templateUrl: 'views/dashboard.html', controller: Dashboard});
+	$routeProvider.when('/pos', {templateUrl: 'views/pos.html', controller: Orders});
+	$routeProvider.when('/orders', {templateUrl: 'views/orders.html', controller: Orders});
+	$routeProvider.when('/vendororders', {templateUrl: 'views/vendororders.html', controller: Orders});
     $routeProvider.when('/products', {templateUrl: 'views/products.html', controller: Products});
-    $routeProvider.when('/orders', {templateUrl: 'views/orders.html', controller: Orders});
+    $routeProvider.when('/promotions', {templateUrl: 'views/promotions.html', controller: Products});
+    $routeProvider.when('/reports', {templateUrl: 'views/reports.html', controller: Orders});
     $routeProvider.otherwise({redirectTo: '/dashboard'});
   }]);
